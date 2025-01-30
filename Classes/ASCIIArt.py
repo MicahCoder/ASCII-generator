e3acedfa5f1ad0ad941a009ac7e0fe923d032515
MicahCoder <123602038+MicahCoder@users.noreply.github.com>
2025-01-30 09:54:03 -0700

    Comments

--- Classes/ASCIIArt.py
+++ Classes/ASCIIArt.py
@@ -7,24 +7,13 @@ from rembg import remove
 ASCIIGradient = '''$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.     '''
 gradientLength = len(ASCIIGradient)
 class ASCIIRenderer:
-
+    # defines the ASCIIRenderer Object
+    # @param img - the image, as a PIL Image object
+    # @param width - the desired amount of letters across the screen to render the image
+    # @param removeBackground - if true it removes the image background
+    # @param hScaleFactor - Determines the stretch of the height, this is used to counteract the fact characters are taller than they are wide.
+    # @return none
     def __init__(self, img,width :int,removeBackground:bool, hScaleFactor:float):
-        self.hScaleFactor = hScaleFactor
-        self.width = width
-        self.removeBackground = removeBackground
-        if(isinstance(img,Image)):
-            
-            self.img = img
-        elif(isinstance(img,str)):
-            self.img = Image.open(img)
-        elif(isinstance):
-            self.img = Image.fromarray(img)
-        wFactor = width/float(self.img.size[0])
-        self.height = int(float(self.img.size[1])*wFactor*hScaleFactor)
-        self.img = self.img.resize((width,self.height),Image.Resampling.LANCZOS)
-        if(removeBackground):
-            self.img = remove(self.img)
-    def __init__(self, img:numpy.array,width :int,removeBackground:bool, hScaleFactor:float):
         self.hScaleFactor = hScaleFactor
         self.width = width
         self.removeBackground = removeBackground
@@ -34,16 +23,25 @@ class ASCIIRenderer:
         self.img = self.img.resize((width,self.height),Image.Resampling.LANCZOS)
         if(removeBackground):
             self.img = remove(self.img)
-    #@param val, value between 0 and 1, returns ascii constant.
+    # Takes in a value and returns a character of coresponding lightness.
+    # @param val - value between 0 and 1, higher returns a lighter character.
+    # @return chr, ranging from lightest to darkest
     def charFromVal(self, val:float) -> chr:
         return ASCIIGradient[int(val*gradientLength)-1]
-    #Uses NTSC method divided by 255
+    # Takes a rgb pixel value and returns it as a gray-scale value between 0 and 1.
+    # Uses NTSC method divided by 255
+    # @param tup - a tuple object of RGB
+    # @return grayscale value between 0 - 1
     def rgbToGrayScale(self, tup:tuple) -> float:
         return (0.00117254901*tup[0]+0.00230196078*tup[1]+0.00044705882*tup[2])
+    # Converts localized PIL image to chr array,
+    # @return array of ASCII letters
     def imageToASCII(self) -> list:
         arr = numpy.array(self.img)
         return [[self.charFromVal(self.rgbToGrayScale(arr[py][px])) for px in range(0,self.width)]for py in range(0,self.height)]
-    def __str__(self):
+    # Turns ASCII array into a printable statement
+    # @return printable string
+    def __str__(self) -> str:
         arr = self.imageToASCII()
         out =''
         for py in range(0,self.height):
@@ -51,19 +49,35 @@ class ASCIIRenderer:
             for px in range(0,self.width):
                 out += arr[py][px]
         return out
-    def writeToFile(self, filePath:str):
+    # Writes ASCII text to file
+    # @param filePath - A string describing filePath
+    # @return none
+    def writeToFile(self, filePath:str) -> None:
         out = open(filePath, 'w')
         out.write(str(self))
         out.close()
+    # Updates the window with image from image supplier
+    # Parameters set in displayToVideo
+    # @return none
     def updateWindow(self):
         self.__init__(self.imageSupplier(), self.width , self.removeBackground,self.hScaleFactor)
         self.l.config(text=str(self))
         self.l.pack()
         self.root.after(1000//self.FPS,self.updateWindow)
+    #Updates the font size and width of image when slider updates.
     #Slider code inspired by https://www.geeksforgeeks.org/python-tkinter-scale-widget/
+    # @param length - takes in slider value.
+    # @return none
     def sliderChanged(self,length):
         self.width = int(length)
         self.l.config(font =("Courier", 2000//self.width))
+    #Creates a window that updates from an image supplier. 
+    # inspired by https://www.geeksforgeeks.org/python-tkinter-text-widget/ for tkinter widget
+    # @param image_supplier - lambda function that supplies a PIL Image
+    # @param FPS - frames per second (It may be slower becuase limits on processing speed)
+    # @param width - the width of the window
+    # @param height - height of the window
+    # @return none
     def displayVidToWindow(self,image_supplier,FPS:int,width:int, height:int):
         self.FPS = FPS
         self.root = tk.Tk()
@@ -80,7 +94,11 @@ class ASCIIRenderer:
         self.imageSupplier = image_supplier
         self.root.after(0,self.updateWindow)
         self.root.mainloop()
-        
+    #Creates a window showing ASCII image
+    # inspired by https://www.geeksforgeeks.org/python-tkinter-text-widget/ for tkinter widget
+    # @param width - the width of the window
+    # @param height - height of the window
+    # @return none
     def displayPicToWindow(self,width:int,height:int):
         root = tk.Tk()
         root.geometry(str(width)+"x" + str(height))
